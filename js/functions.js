21fab946f5d0787b41b93fa5e8c736c9742f81f8
Michael Palmer <michaelfpalmer@gmail.com>
2018-08-06 16:46:25 -0700

    added js

---
+++ js/functions.js
@@ -0,0 +1,5 @@
+var imgtag = "<img src='http://academyartunews.com/content/newspaper/2015/04/selfie_vs_self-port/_jcr_content/par/column_control/col_two/image.img.jpg/1428451475676.jpg'>"
+
+function displayImage() {
+  document.getElementById('something').innerHTML=imgtag
+}
